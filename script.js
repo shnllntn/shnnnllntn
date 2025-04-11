7b2753caabc5be66faf4f762dd6e8fcc66adcb46
shnllntn <sllantino1523qc@student.fatima.edu.ph>
2025-04-11 17:21:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("header").style.opacity = "1";
+    document.querySelector("header").style.transform = "translateY(0)";
+    document.querySelector("main").style.opacity = "1";
+    document.querySelector("main").style.transform = "translateY(0)";
+    document.querySelector("footer").style.opacity = "1";
+    document.querySelector("footer").style.transform = "translateY(0)";
+  });
